468cda51a4c646cf6a3ff935092369f9d1a69cd6
Midhun Biju <166794539+midhunbiju102003@users.noreply.github.com>
2024-12-17 14:37:17 +0530

    Nameofstudents.php

---
+++ Nameofstudents.php
@@ -0,0 +1,10 @@
+<?php
+$a=array("Anjala","Christeena","Blessey","Angela");
+print_r($a);
+echo("<br>Ascending order: ");
+sort($a);
+print_r($a);
+echo("<br>Descending order: ");
+rsort($a);
+print_r($a);
+?>
